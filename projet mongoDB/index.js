4677f5e35f2965cea86684094cb745c50e280b95
AlexandreRosset <alexandre.rosset97@laposte.net>
2018-01-15 09:34:40 +0100

    création de user

--- projet mongoDB/index.js
+++ projet mongoDB/index.js
@@ -2,12 +2,13 @@ var http = require("http");
 var app = require('express')();
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 mongoose.connect('mongodb://localhost:27017/DelormeRosset');
 
 var Schema = require('./schema');
 
 httpserver = http.createServer(app);
-
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     Schema.User.find({}, function (err, usrs) {
@@ -18,6 +19,18 @@ app.get('/user/:id', function (req, res) {
     Schema.User.findById(req.params.id , function (err, usrs) {
         res.json(usrs);
     }).exec();
+});
+app.post('/user', function (req, res) {
+    var usr = new Schema.User({
+        nom: req.body.nom,
+        prenom: req.body.prenom,
+        naissance: req.body.dateNaissance,
+        login: req.body.login,
+        password: req.body.password,
+        client: false,
+        groupe: []
+    });
+    usr.save();
 });
 
 
